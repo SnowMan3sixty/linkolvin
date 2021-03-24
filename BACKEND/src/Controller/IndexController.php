be571f52c56d07fe9da1c7cdd894fb7f58147fcb
SnowMan3sixty <73338167+SnowMan3sixty@users.noreply.github.com>
2021-03-24 14:13:18 +0100

    [WIP]company view

--- BACKEND/src/Controller/IndexController.php
+++ BACKEND/src/Controller/IndexController.php
@@ -13,15 +13,31 @@ class IndexController extends AbstractController
 {
     #[Route('/', name: 'index')]
     public function index()
-    {        
-        return $this->render('index/index.html.twig');
+    {    
+        if($this->getUser())    {
+            if(in_array('ROLE_COMPANY', $this->getUser()->getRoles(), true) || in_array('ROLE_ADMIN', $this->getUser()->getRoles(), true) ){
+                return $this->render('index/index.html.twig');
+            }
+            else{
+                return $this->redirect('http://localhost:8080');
+            }     
+        }
+        else{
+            return $this->redirectToRoute('app_login');
+        }
+           
     }
     #[Route('/logged', name: 'logged')]
     public function logged()
     {               
-       if(in_array('ROLE_COMPANY', $this->getUser()->getRoles(), true) && !($this->getUser()->isVerified())){
-            return $this->redirectToRoute('empresa_new');
-       }
+       if(in_array('ROLE_COMPANY', $this->getUser()->getRoles(), true) ){
+            if(!($this->getUser()->isVerified())){
+                return $this->redirectToRoute('empresa_new');
+            }
+            else{
+                return $this->redirectToRoute('index');
+            }        
+       }     
        else{
             return $this->redirect('http://localhost:8080');
        }
